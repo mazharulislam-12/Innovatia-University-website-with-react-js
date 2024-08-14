b119164b9d795fa34e1794e4d6e5a28d892d168a
mazharulislam-12 <505mazharulislam@gmail.com>
2024-08-14 15:25:58 +0600

    program section crate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
+import Program from "./Components/Program/Program"
+import Title from "./Components/Title/Title"
 
 const App = () => {
   return (
     <div>
 
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='Our Program' title='What We Offer' />
+        <Program />
+      </div>
 
     </div>
   )
