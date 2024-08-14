82b50f985b184f9946242881652835114904ba04
mazharulislam-12 <505mazharulislam@gmail.com>
2024-08-14 14:28:50 +0600

    hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,14 @@
 import './Hero.css'
+import darkArrow from '../../assets/dark-arrow.png';
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>We Ensure better eduction for a better world</h1>
+            <p>Innovatia University fosters innovation, creativity, and technology-driven education, empowering students to excel in global leadership. Our programs blend hands-on learning with innovative thinking for extraordinary achievements.</p>
+            <button className='btn '>Explore More <img src={darkArrow} alt="" /> </button>
+        </div>
     </div>
   )
 }
