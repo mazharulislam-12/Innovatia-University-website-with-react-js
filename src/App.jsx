2e9ff6d7ffdf7be4076681118631f6f81ae5a057
mazharulislam-12 <505mazharulislam@gmail.com>
2024-08-15 15:41:33 +0600

    contact sectiona and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import About from "./Components/About/About"
 import Campus from "./Components/Campus/Campus"
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer/Footer"
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 import Program from "./Components/Program/Program"
@@ -20,6 +22,9 @@ const App = () => {
         <Campus/>
         <Title subTitle='TESTIMONIALS' title='What Student Says' />
         <Testimonial/>
+        <Title subTitle='Contact Us' title='Get in Touch' />
+        <Contact/>
+        <Footer/>
       </div>
 
     </div>
